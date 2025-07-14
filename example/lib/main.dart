02256dc00210f7466d4776a3bfdcafb52dc29d3f
박기현 <kihyun1998@naver.com>
2025-07-14 15:44:32 +0900

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Table Plus Example',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
